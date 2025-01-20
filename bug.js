9b1c1bb22f51c75ae4469b7a87c87f3a9f27d0a1
BugHunter-X <askzenai@gmail.com>
2025-01-20 10:56:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Example of Tailwind classes that might lead to unexpected behavior
+<div class="bg-gray-100 hover:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
+  <!-- Content -->
+</div>
+```
